275ffa37b60bbdc4a937b8aee67752fe3425898f
ojungootieno <ojungootieno@gmail.com>
2026-07-14 00:33:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ const app_id = "33OpwxLFrlpIGDluvvUiu";
 
 function loginWithDeriv() {
     window.location.href =
-"https://oauth.deriv.com/oauth2/authorize?app_id=" + app_id + "&l=EN";
+        `https://oauth.deriv.com/oauth2/authorize?app_id=${app_id}`;
 }
 
-// Check if user returned from Deriv
+// Check if user has returned from Deriv
 window.onload = function () {
-    const url = window.location.href;
 
-    if (url.includes("token=") || url.includes("code=")) {
-    localStorage.setItem("deriv_login", "true");
-    window.location.href = "dashboard.html";
+    const hash = window.location.hash;
+
+    if (hash.includes("access_token=")) {
+
+        const params = new URLSearchParams(hash.substring(1));
+
+        const token = params.get("access_token");
+
+        if (token) {
+
+            localStorage.setItem("deriv_token", token);
+
+            window.location.href = "dashboard.html";
+
+        }
     }
+
 };
