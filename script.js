cd09bfe6402f90b6898e4d48fbe59e1a2b48382e
ojungootieno <ojungootieno@gmail.com>
2026-07-13 23:47:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const app_id = "33OpwxLFrlpIGDluvvUiu";
 
 function loginWithDeriv() {
-  const redirect = window.location.origin + "/dashboard.html";
-
-  const url =
-    "https://oauth.deriv.com/oauth2/authorize?app_id=" +
-    app_id +
-    "&redirect_uri=" +
-    encodeURIComponent(redirect);
-
-  window.location.href = url;
+    window.location.href =
+        "https://oauth.deriv.com/oauth2/authorize?app_id=" + app_id;
 }
+
+// Check if user returned from Deriv
+window.onload = function () {
+    const url = window.location.href;
+
+    if (url.includes("token=") || url.includes("code=")) {
+        alert("Login successful! Welcome to OTI Trading.");
+    }
+};
